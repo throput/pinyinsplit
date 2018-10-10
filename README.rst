c8fda93f5a0fb4a19578c306635348627a3af89a
Thomas Lee <thomaslee@throput.com>
2018-10-10 11:52:52 +0800

    add license, readme, and setup file

---
+++ README.rst
@@ -0,0 +1,5 @@
+pinyinsplit
+===========
+
+This is a Python library for splitting a Chinese Pinyin phrase into possible 
+permutations of valid Pinyin words.
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='pinyinsplit',
+    version='0.1',
+    description='A Python library to split a Chinese Pinyin phrase into possible permuntations of Chinese Pinyin words',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+    keywords='chinese pinyin',
+    url='https://gitlab.com/neuronetic/pinyinsplit',
+    author='Thomas Lee',
+    author_email='thomaslee@throput.com',
+    pymodule='pinyinsplit',
+    install_requires=[
+        'pygtrie',
+    ],
+    include_page_data=True,
+    zip_safe=False
+)
