e500da5b4b37e4d7762790825e1efd1e6e0f4765
Thomas Lee <thomaslee@throput.com>
2018-10-11 01:00:14 +0800

    fix bugs in setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,15 @@ def readme():
 
 setup(
     name='pinyinsplit',
-    version='0.1.3',
+    version='0.1.4',
     author='Thomas Lee',
     author_email='thomaslee@throput.com',
     description='A Python library to split a Chinese Pinyin phrase into possible permutations of Chinese Pinyin words',
     long_description=readme(),
     url='https://github.com/throput/pinyinsplit',
-    packages=find_packages(),
+    py_modules = ['pinyinsplit'],
+    license='MIT',
+    install_requires=['pygtrie>=2.3'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
